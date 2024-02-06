a8365e2ff3080cc695d3a4cdc44ab24ade280a8c
josemelendez77 <141665270+josemelendez77@users.noreply.github.com>
2024-02-05 20:29:07 -0700

    Update Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Reactcomponent as logo} from '../../assests/react.svg'
+class login extends React.Component{
+    state={
+        email:'',
+        pwd:''
+    }
+    handleChange = (e) =>{
+        const {name,value} = e.target
+        this.setState({[name]:value})
+
+    }
+
+    handlSubmit = (e) =>{
+
+    }
+    render(){
+        return(
+            <div> 
+                <div>
+                     <logo/>
+                </div>  
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type='email' name='email' placeholder='email...' required onChange={this.handlSubmit}/>
+                        <input type='password' name='pwd' placeholder='password...' required onChange={this.handlSubmit}/>
+                        <button onSubmit={this.handlSubmit}>Log in </button>
+                    </form>
+                    </div>  
+            </div>
+        )
+    }
+}
+
+export default login;
