bdf3a1e643067a5a1e88b7535a2bae725f091a88
josemelendez77 <141665270+josemelendez77@users.noreply.github.com>
2024-02-05 20:31:28 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 
-// import Header from "./components/header/index";
-// import Footer from "./components/footer/index";
+import Header from "./components/header/index";
+import Footer from "./components/footer/index";
 
 function App() {
-  return (
-    <>
-      {/* <Header /> */}
-      <Outlet />
-      {/* <Footer /> */}
-    </>
-  );
+  return(
+    <div>
+      <Switch>
+        <Route exact path='/' component={login}/>
+      </Switch>
+    </div>
+  )
 }
 
 export default App;
